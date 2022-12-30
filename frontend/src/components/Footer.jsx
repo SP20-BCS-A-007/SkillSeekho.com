3c3d8b00720858ddbed2ae0c7a8450ee5d5b21b3
SP20-BCS-A-007 <sp20-bcs-007@cuilahore.edu.pk>
2022-12-31 01:53:39 +0500

    Update Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer class="text-center fs-6 fw-bold text-white">
       <div class="text-center p-3" style={{backgroundColor: '#FFC200',}}>
-        © 2020 Copyright: 
+        © 2022 Copyright: 
         <a className="text-white" style={{textDecoration: 'none'}} href="/"> SkillSeekho.com</a>
       </div>
     </footer>
