c6824c641a028d8b70931464de08d9a0e8aee6ca
SP20-BCS-A-007 <sp20-bcs-007@cuilahore.edu.pk>
2023-01-03 15:49:22 +0500

    Update Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer class="text-center fs-6 fw-bold text-white" style={{bottom: "0"}}>
+    <footer class="text-center fs-6 fw-bold text-white">
       <div class="text-center p-3 bg-secondary" >
         © 2022 Copyright: 
         <a className="text-white" style={{textDecoration: 'none'}} href="/"> SkillSeekho.com</a>
